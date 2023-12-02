f8340f68c9f1a708f61cf942bed83f3c2db1d125
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2023-12-02 22:36:20 +0530

    created best practice code using jest prettier husky

---
+++ index.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+
+const add = (value1, value2) => {
+  return value1 + value2;
+};
+
+const sub = (value1, value2) => {
+  return value1 - value2;
+};
+
+const getTodo = async (url) => {
+  try {
+    const result = await axios.get(url);
+    return result.data;
+  } catch (error) {
+    console.log("error2", error.message);
+    throw {
+      message: error.message,
+      code: error.code,
+      succeded: false,
+    };
+  }
+};
+
+module.exports = { add, sub, getTodo };
---
+++ test/index.test.js
@@ -0,0 +1,38 @@
+const { add, sub, getTodo } = require("../index");
+describe(" Test case for index.js file", () => {
+  test("test case for add function", () => {
+    const result = add(1, 2);
+    expect(result).toBe(3);
+  });
+
+  test("test case for sub function", () => {
+    const result = sub(2, 1);
+    expect(result).toBe(1);
+  });
+
+  test("test case for axios call of success", async () => {
+    const response = {
+      userId: 1,
+      id: 1,
+      title: "delectus aut autem",
+      completed: false,
+    };
+    const url = "https://jsonplaceholder.typicode.com/todos/1";
+    const result = await getTodo(url);
+    expect(result).toStrictEqual(response);
+  });
+
+  test("test case for axios call of failure", async () => {
+    const response = {
+      message: "getaddrinfo ENOTFOUND jsonplaceholde.typicode.com",
+      code: "ENOTFOUND",
+      succeded: false,
+    };
+    const url = "https://jsonplaceholde.typicode.com/todos/1";
+    try {
+      await getTodo(url);
+    } catch (error) {
+      expect(error).toStrictEqual(response);
+    }
+  });
+});
